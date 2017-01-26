a62b64b4b0f044396762302c0d4f6053d3abcb0b
Ramon Valk <valkramon1@gmail.com>
2017-01-26 13:46:01 +0100

    Bugfix, bestand werd op de server niet goed gelezen.

--- sshTool/sshTool.sh
+++ sshTool/sshTool.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
-sshServer=move4mobile@192.168.13.97
+sshServer=move4mobile@192.168.13.12
+dirServer="/tmp/"
 echo "Checking arguments..."
 if [[ "$1" -eq "-a" ]] && [[ "$2" == *".apk" ]]; then
 	echo "-a flag found, Copying over to server..."
-	scp $2 $sshServer:/tmp
+	scp $2 $sshServer:$dirServer
 	app="$(echo $2 | rev | cut -d'/' -f-1 | rev)"
-	
+	app=$dirServer$app
 	shift 2
 	ssh $sshServer "PATH=/usr/local/bin:$PATH && /usr/local/bin/mdInstaller -a $app $@"
 	echo "$@"
+	exit 0
 fi
 ssh $sshServer "PATH=/usr/local/bin:$PATH && /usr/local/bin/mdInstaller $@"
+exit 0
